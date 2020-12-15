09518308afe6c7012938bbcc34b7a91a618f3959
Jarl-Penguin <jarlpenguin@outlook.com>
2020-12-15 08:46:43 +0300

    cedric: Don't remove MotoDoze
    * It's not built for cedric anymore
    
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: Ib86e5cdeb9c4bc57776693226b88a06a645b5846

--- configs/check_device.sh
+++ configs/check_device.sh
@@ -2,7 +2,6 @@
 
 SKU=`getprop ro.boot.hardware.sku`
 
-rm -rf /system_root/system/priv-app/MotoDoze
 if [ "$SKU" == "XT1677" ]; then
     mv /vendor/etc/thermal-engine-India.conf /vendor/etc/thermal-engine.conf
 fi
